77cfb90c99d6b66f50a4b3ebe2ee6bebc3cec699
Victor Banerjee <recervictory@gmail.com>
2022-09-12 11:43:26 +0530

    C5W1 - Style Components

--- C5 - React Basics/W01 React Compoents/Your first component/reactlab/src/App.js
+++ C5 - React Basics/W01 React Compoents/Your first component/reactlab/src/App.js
@@ -4,9 +4,9 @@ function App() {
   return (
     <div className="App">
       <h1>Task: Add three Card elements</h1>
-      <Card h2="First card's h2" h3="First card's h3" />
-      <Card h2="Second card's h2" h3="Second card's h3" />
-      <Card h2="Third card's h2" h3="Third card's h3" />
+      <Card h2="First card's h2" h3="First card's h3" backgroundColor='#FCB900'/>
+      <Card h2="Second card's h2" h3="Second card's h3" backgroundColor='#00D084'/>
+      <Card h2="Third card's h2" h3="Third card's h3" backgroundColor='#9900EF'/>
     </div>
   );
 }
--- C5 - React Basics/W01 React Compoents/Your first component/reactlab/src/components/Card.js
+++ C5 - React Basics/W01 React Compoents/Your first component/reactlab/src/components/Card.js
@@ -1,6 +1,15 @@
 const Card = (props) => {
+    const CardStyle = {
+        textAlign: 'center',
+        backgroundColor: props.backgroundColor,
+        padding: '10px',
+        border: '1px solid',
+        margin: '10px'
+
+    }
+
     return (
-        <div className="card">
+        <div className="card" style={CardStyle}>
         <h2>{props.h2}</h2>
         <h3>{props.h3}</h3>
         </div>
