fd883411899307b3bf42d5ae0e608083871693ed
Victor Banerjee <recervictory@gmail.com>
2022-09-13 16:40:46 +0530

    C5W3 - React Router Dom Applied

--- C5 - React Basics/W03 Linking Routing and Assets/tutorial/src/App.jsx
+++ C5 - React Basics/W03 Linking Routing and Assets/tutorial/src/App.jsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
+import Homepage from "./Homepage";
+import AboutMe from "./AboutMe";
+import Contact from "./Contact";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <BrowserRouter>
+      <div className="App">
+        <nav className="navbar">
+          <Link to="/" className="nav-item">
+            Homepage
+          </Link>
+          <Link to="/about-me" className="nav-item">
+            About me
+          </Link>
+          <Link to="/contact" className="nav-item">
+            Contact
+          </Link>
+        </nav>
+        <section>
+          <Routes>
+            <Route path="/" element={<Homepage />}></Route>
+            <Route path="/about-me" element={<AboutMe />}></Route>
+            <Route path="/contact" element={<Contact />}></Route>
+          </Routes>
+        </section>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ C5 - React Basics/W03 Linking Routing and Assets/tutorial/src/Homepage.jsx
@@ -0,0 +1,7 @@
+const Homepage = () => {
+    return (
+        <h1>Home Page</h1>
+    );
+}
+
+export default Homepage;
