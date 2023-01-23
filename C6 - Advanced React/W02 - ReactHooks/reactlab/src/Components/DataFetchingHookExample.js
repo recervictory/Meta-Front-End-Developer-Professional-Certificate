c9cd6f4b87d4f8d4fd3782f66280d9bb6f6d3617
Victor Banerjee <recervictory@gmail.com>
2023-01-23 12:59:57 +0000

    C6 - Object.keys.length used to custom fetch

--- C6 - Advanced React/W02 - ReactHooks/reactlab/src/Components/DataFetchingHookExample.js
+++ C6 - Advanced React/W02 - ReactHooks/reactlab/src/Components/DataFetchingHookExample.js
@@ -1,24 +1,28 @@
-import React, {useState,useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
 const DataFetchingHookExample = () => {
-    const [data, setData] = useState({});
+  const [data, setData] = useState({});
 
-    useEffect(() => {
-        fetch(`https://api.coindesk.com/v1/bpi/currentprice.json`)
-        .then((response) => response.json())
-        .then((json) => setData(json.bpi.USD))
-        .catch( (error) => console.log(error));
-}, []);
-  return (
-    <div> 
-      <h1>Current BTC/USD data</h1> 
-      <p>Code: {data.code}</p> 
-      <p>Symbol: {data.symbol}</p> 
-      <p>Rate: {data.rate}</p> 
-      <p>Description: {data.description}</p> 
-      <p>Rate Float: {data.rate_float}</p> 
-    </div> 
-  )
-}
+  const fetchData = () => {
+    fetch(`https://api.coindesk.com/v1/bpi/currentprice.json`)
+      .then((response) => response.json())
+      .then((json) => setData(json.bpi.USD))
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return Object.keys(data).length > 0 ? (
+    <div >
+      <h1>Current BTC/USD data</h1>
+      <p>Code: {data.code}</p>
+      <p>Symbol: {data.symbol}</p>
+      <p>Rate: {data.rate}</p>
+      <p>Description: {data.description}</p>
+      <p>Rate Float: {data.rate_float}</p>
+    </div>
+  ) : (<h1>Data pending...</h1>);
+};
 
 export default DataFetchingHookExample;
